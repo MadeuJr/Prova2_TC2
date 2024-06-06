f691bd9b01a6c449c8f134abd52521bc38381b1a
Amadeu José Andrade Jr <amadeujajunior@gmail.com>
2024-06-06 09:25:32 -0300

    inicialização servidor node com TS

---
+++ Prova/backend/src/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+const port = 2981;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Access: http://localhost:${port}`))
