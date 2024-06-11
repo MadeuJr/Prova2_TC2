88397f17d93f0dbc3d1ff8e2a4e71c582dfbba41
Amadeu José Andrade Jr <amadeujajunior@gmail.com>
2024-06-11 12:52:12 -0300

    Adição das variaveis de ambiente para o Backend e continuação do front

--- Prova/frontend/src/app/app.routes.ts
+++ Prova/frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomeComponent, pathMatch:'full'},
+    // {path: 'comp2', component: Rota2Component, pathMatch: 'full'}
+];
